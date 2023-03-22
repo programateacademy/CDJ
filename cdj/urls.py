69bb5a0bda5bb4b3da51f0f17a6f229d5d4ad2f5
Andrea Elbittar <andreaelbittarl@gmail.com>
2023-03-22 10:17:12 -0500

    urls

--- cdj/urls.py
+++ cdj/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('login/', include('login.urls', namespace="login")),
     path('search_consejos/', views.search_consejos, name='search_consejos'),
+    path('consejos/<int:consejo_id>/', views.detalle_consejo, name='detalle_consejo_local'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
