509c1e64677c1e5df117b1c8cc12e96ddca16616
rosaidabhz <rosaidabhz@gmail.com>
2023-03-22 12:58:14 -0500

    html and styles home, logi

--- cdj/urls.py
+++ cdj/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('login/', include('login.urls', namespace="login")),
-    path('search_consejos/', views.search_consejos, name='search_consejos'),
+    path('search_consejos/', views.search_consejos, name='search_consejos')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
